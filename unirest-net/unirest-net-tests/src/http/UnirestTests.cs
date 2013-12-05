ac963e161d0d2b55710466da91eeef1ac5a695f2
Lionel Ringenbach <ucodia@live.com>
2013-12-05 16:22:55 +0100

    Renamed Unirest methods to use Pascal case

--- unirest-net/unirest-net-tests/src/http/UnirestTests.cs
+++ unirest-net/unirest-net-tests/src/http/UnirestTests.cs
@@ -21,21 +21,21 @@ namespace unirest_net.http
         [Test]
         public static void Unicorn_Should_Return_Correct_Verb()
         {
-            Unirest.get("http://localhost").HttpMethod.Should().Be(HttpMethod.Get);
-            Unirest.post("http://localhost").HttpMethod.Should().Be(HttpMethod.Post);
-            Unirest.delete("http://localhost").HttpMethod.Should().Be(HttpMethod.Delete);
-            Unirest.patch("http://localhost").HttpMethod.Should().Be(new HttpMethod("PATCH"));
-            Unirest.put("http://localhost").HttpMethod.Should().Be(HttpMethod.Put);
+            Unirest.Get("http://localhost").HttpMethod.Should().Be(HttpMethod.Get);
+            Unirest.Post("http://localhost").HttpMethod.Should().Be(HttpMethod.Post);
+            Unirest.Delete("http://localhost").HttpMethod.Should().Be(HttpMethod.Delete);
+            Unirest.Patch("http://localhost").HttpMethod.Should().Be(new HttpMethod("PATCH"));
+            Unirest.Put("http://localhost").HttpMethod.Should().Be(HttpMethod.Put);
         }
 
         [Test]
         public static void Unicorn_Should_Return_Correct_URL()
         {
-            Unirest.get("http://localhost").URL.OriginalString.Should().Be("http://localhost");
-            Unirest.post("http://localhost").URL.OriginalString.Should().Be("http://localhost");
-            Unirest.delete("http://localhost").URL.OriginalString.Should().Be("http://localhost");
-            Unirest.patch("http://localhost").URL.OriginalString.Should().Be("http://localhost");
-            Unirest.put("http://localhost").URL.OriginalString.Should().Be("http://localhost");
+            Unirest.Get("http://localhost").URL.OriginalString.Should().Be("http://localhost");
+            Unirest.Post("http://localhost").URL.OriginalString.Should().Be("http://localhost");
+            Unirest.Delete("http://localhost").URL.OriginalString.Should().Be("http://localhost");
+            Unirest.Patch("http://localhost").URL.OriginalString.Should().Be("http://localhost");
+            Unirest.Put("http://localhost").URL.OriginalString.Should().Be("http://localhost");
         }
     }
 }
--- unirest-net/unirest-net/src/http/Unirest.cs
+++ unirest-net/unirest-net/src/http/Unirest.cs
@@ -7,27 +7,27 @@ namespace unirest_net.http
     public class Unirest
     {
         // Should add overload that takes URL object
-        public static HttpRequest get(string url)
+        public static HttpRequest Get(string url)
         {
             return new HttpRequest(HttpMethod.Get, url);
         }
 
-        public static HttpRequest post(string url)
+        public static HttpRequest Post(string url)
         {
             return new HttpRequest(HttpMethod.Post, url);
         }
 
-        public static HttpRequest delete(string url)
+        public static HttpRequest Delete(string url)
         {
             return new HttpRequest(HttpMethod.Delete, url);
         }
 
-        public static HttpRequest patch(string url)
+        public static HttpRequest Patch(string url)
         {
             return new HttpRequest(new HttpMethod("PATCH"), url);
         }
 
-        public static HttpRequest put(string url)
+        public static HttpRequest Put(string url)
         {
             return new HttpRequest(HttpMethod.Put, url);
         }
